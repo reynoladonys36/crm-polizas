cb746c9e0fc536c0fd11ba4f90c0a920efc50bd3
reynoladonys36 <reynoladonys36@gmail.com>
2026-09-10 22:15:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -309,6 +309,6 @@ def clasificar(peticion: PeticionClasificar,
     except json.JSONDecodeError:
         return RespuestaClasificar(intencion="Otro", confianza=0.0,
                                    motivo="respuesta_no_json")
-            except Exception:
+    except Exception:
         return RespuestaClasificar(intencion="Otro", confianza=0.0,
                                    motivo="error_llm")
